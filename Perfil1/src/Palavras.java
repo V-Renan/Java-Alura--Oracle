69d70fcf891d02c5c88d88f68af6149cc389f0d8
V-Renan <victorrenan2018@gmail.com>
2024-04-04 02:54:32 -0300

    Perfil1
    concatenando

---
+++ Perfil1/src/Palavras.java
@@ -0,0 +1,17 @@
+/**
+ * @author Victor$
+ * @date 4/4/2024$
+ * Description:
+ */
+public class Palavras {
+    public static void main(String[] args) {
+        char letra;
+        String palavra;
+
+        letra = 'J';
+        palavra = "ava";
+        String mensagem = letra + palavra;
+
+        System.out.println(mensagem);
+    }
+}
