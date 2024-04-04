06bef70ba59aeb206f82429f63d3189a80a2bb4e
V-Renan <victorrenan2018@gmail.com>
2024-04-04 02:57:01 -0300

    Perfil1
    calculando valor da compra

---
+++ Perfil1/src/Produto.java
@@ -0,0 +1,15 @@
+/**
+ * @author Victor$
+ * @date 4/4/2024$
+ * Description:
+ */
+public class Produto {
+    public static void main(String[] args) {
+        double precoProduto = 150.90;
+        int quantidade = 2;
+
+        double valorTotal = precoProduto * quantidade;
+        System.out.println("Valor total da compra: R$" + valorTotal);
+
+    }
+}
