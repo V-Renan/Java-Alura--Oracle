73f162b34c6d0061c9afaaba88148d131fd161df
V-Renan <victorrenan2018@gmail.com>
2024-04-04 02:51:10 -0300

    Perfil1
    Casting double para integer

---
+++ Perfil1/src/Casting.java
@@ -0,0 +1,12 @@
+/**
+ * @author Victor$
+ * @date 4/4/2024$
+ * Description:
+ */
+public class Casting {
+    public static void main(String[] args) {
+        double valorDouble = 7.14;
+        int valorInteiro = (int) valorDouble;
+        System.out.println("Resultado do casting: " + valorInteiro);
+    }
+}
