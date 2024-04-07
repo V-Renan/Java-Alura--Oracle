0d5d15a9bcb29cd846d387017f4e7c09072edba8
V-Renan <victorrenan2018@gmail.com>
2024-04-07 18:28:51 -0300

    Perfil1
    fatorial

---
+++ Perfil1/src/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @author Victor$
+ * @date 4/7/2024$
+ * Description:
+ */
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para calcular o fatorial: ");
+        int numero = scanner.nextInt();
+        long fatorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        scanner.close();
+    }
+}
