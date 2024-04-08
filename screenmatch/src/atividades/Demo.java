8f743be9ace160e7daaef502ab3b7103d1b7d3d1
V-Renan <victorrenan2018@gmail.com>
2024-04-08 03:26:54 -0300

    POO
    atividade hello world

---
+++ screenmatch/src/atividades/Demo.java
@@ -0,0 +1,14 @@
+package atividades;
+
+/**
+ * @author Victor$
+ * @date 4/8/2024$
+ * Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Saudacao mensagem = new Saudacao();
+
+        mensagem.exibirSaudacao();
+    }
+}
---
+++ screenmatch/src/atividades/Saudacao.java
@@ -0,0 +1,12 @@
+package atividades;
+
+/**
+ * @author Victor$
+ * @date 4/8/2024$
+ * Description:
+ */
+public class Saudacao {
+    void exibirSaudacao() {
+        System.out.println("Hello World!");
+    }
+}
