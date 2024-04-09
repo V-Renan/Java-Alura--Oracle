9322179b72b11e47d80f8b48b8dd8e0a83496ee6
V-Renan <victorrenan2018@gmail.com>
2024-04-08 23:32:10 -0300

    POO
    getters and setters

---
+++ desafios/src/desafio4/Demo.java
@@ -0,0 +1,15 @@
+package desafio4;
+
+import javax.swing.*;
+
+/**
+ * @author Victor$
+ * @date 4/8/2024$
+ * Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("God of War - Ragnarok", 250.00);
+        produto1.aplicarDesconto(5);
+    }
+}
--- desafios/src/desafio4/Produto.java
+++ desafios/src/desafio4/Produto.java
@@ -9,4 +9,33 @@ public class Produto {
     private String nome;
     private double preco;
 
+    public Produto() {
+
+    }
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void aplicarDesconto(double valor) {
+        double valorParcial = preco * (valor / 100);
+        preco -= valorParcial;
+        System.out.printf("Valor do desconto: R$%.2f \nTotal a ser pago: %.2f", valorParcial, preco);
+    }
 }
