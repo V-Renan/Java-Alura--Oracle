a9b823e88a764e513924d54b14fb12b7629b3fb3
V-Renan <victorrenan2018@gmail.com>
2024-04-09 00:49:11 -0300

    POO
    getters and setters

---
+++ desafios/src/desafio5/Demo.java
@@ -0,0 +1,14 @@
+package desafio5;
+
+/**
+ * @author Victor$
+ * @date 4/8/2024$
+ * Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("Michael Jackson", 9, 7, 5);
+        System.out.println("Nome: " + aluno1.getNome());
+        aluno1.calcularMedia();
+    }
+}
