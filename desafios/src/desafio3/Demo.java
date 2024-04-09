b43bcfa56a7b8c2d892019af6739acb7d221db53
V-Renan <victorrenan2018@gmail.com>
2024-04-08 22:11:55 -0300

    POO
    getter and setter

---
+++ desafios/src/desafio3/Demo.java
@@ -0,0 +1,13 @@
+package desafio3;
+
+/**
+ * @author Victor$
+ * @date 4/8/2024$
+ * Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        IdadePessoa pessoa1 = new IdadePessoa("Victor", 18);
+        pessoa1.maiorOuMenorIdade();
+    }
+}
---
+++ desafios/src/desafio3/IdadePessoa.java
@@ -0,0 +1,42 @@
+package desafio3;
+
+/**
+ * @author Victor$
+ * @date 4/8/2024$
+ * Description:
+ */
+public class IdadePessoa {
+    private String nome;
+    private int idade;
+    public IdadePessoa() {
+
+    }
+    public IdadePessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void maiorOuMenorIdade() {
+        if (getIdade() >= 18) {
+            System.out.println("A pessoa é maior de idade!");
+        } else {
+            System.out.println("A pessoa é menor de idade!");
+        }
+    }
+}
