1439029f4581446c0864c9ec7a4c92ee27aab6b6
V-Renan <victorrenan2018@gmail.com>
2024-06-04 11:30:15 -0300

    screenmatch
    Springboot

---
+++ screenmatch-spring/src/main/java/br/com/victor/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,23 @@
+package br.com.victor.screenmatch;
+
+import br.com.victor.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args){
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=game+of+thrones&Season=1&apikey=d7c6eae8");
+		System.out.println(json);
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
